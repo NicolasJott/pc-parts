c1609a4c96781470ee80d1119229a3239217e48c
nicolasjott <nicobox2002@gmail.com>
2024-03-18 22:32:29 -0400

    Add products API module

---
+++ pc-parts-app/src/api/products.ts
@@ -0,0 +1,27 @@
+const BASE_URL = import.meta.env.VITE_API_URL;
+import axios from "axios";
+
+export interface IProduct {
+  id: number;
+  name: string;
+  model: string;
+  price: number;
+  category: string;
+  product_image: string;
+  description: string;
+  specifications: string[];
+}
+
+export const getProducts = async () => {
+  const response: { data: IProduct[] } = await axios.get(
+    `${BASE_URL}/products`
+  );
+  return response.data;
+};
+
+export const getProduct = async (id: string) => {
+  const response: { data: IProduct } = await axios.get(
+    `${BASE_URL}/products/${id}`
+  );
+  return response.data;
+};
