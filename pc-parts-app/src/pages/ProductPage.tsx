227d51def00a8ac5f12fc494e64765eec06ef75a
nicolasjott <nicobox2002@gmail.com>
2024-03-18 22:32:44 -0400

    Add ProductPage component

---
+++ pc-parts-app/src/pages/ProductPage.tsx
@@ -0,0 +1,25 @@
+import { Box } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getProduct } from "../api/products";
+import { CustomSpinner } from "../components/CustomSpinner";
+import { ProductPageCard } from "../components/products/ProductPageCard";
+
+export const ProductPage = () => {
+  const { productId } = useParams<{ productId: string }>();
+
+  const query = useQuery({
+    queryKey: ["product", productId],
+    queryFn: () => getProduct(productId || ""),
+  });
+
+  if (query.isLoading) {
+    return <CustomSpinner />;
+  }
+
+  return (
+    <Box mx={"auto"} px={4} maxW={"8xl"}>
+      {query.data && <ProductPageCard product={query.data} />}
+    </Box>
+  );
+};
