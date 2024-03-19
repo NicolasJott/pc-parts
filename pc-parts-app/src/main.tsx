49e41a6d6eea8cd12c489c4057a0da8ce6bd14f5
nicolasjott <nicobox2002@gmail.com>
2024-03-18 22:32:23 -0400

    Add QueryClientProvider for React Query

--- pc-parts-app/src/main.tsx
+++ pc-parts-app/src/main.tsx
@@ -1,14 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { theme } from "./theme/theme";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
