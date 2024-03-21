10720aafb3ea4a3a0315e290e6c35fee848eedea
nicolasjott <nicobox2002@gmail.com>
2024-03-20 23:50:03 -0400

    Add authentication check in Login component

--- pc-parts-app/src/pages/auth/Login.tsx
+++ pc-parts-app/src/pages/auth/Login.tsx
@@ -1,7 +1,14 @@
 import { Box } from "@chakra-ui/layout";
+import { Navigate } from "react-router";
 import { LoginForm } from "../../components/auth/LoginForm";
+import { useAuth } from "../../components/context/AuthContext";
 
 export const Login = () => {
+  const { authenticated } = useAuth();
+
+  if (authenticated) {
+    return <Navigate to="/account" replace />;
+  }
   return (
     <Box>
       <LoginForm />
