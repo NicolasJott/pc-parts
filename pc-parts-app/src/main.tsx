5e226c1f16b6d4b113f05b9020943eac39b5f17f
nicolasjott <nicobox2002@gmail.com>
2024-03-20 20:52:30 -0400

    Add query client options

--- pc-parts-app/src/main.tsx
+++ pc-parts-app/src/main.tsx
@@ -10,6 +10,10 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: 1,
+      staleTime: 5 * 1000,
     },
   },
 });
