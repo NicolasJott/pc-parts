b3f8e39187c2f5a29e44f76ae16a314edb1c8a33
nicolasjott <nicobox2002@gmail.com>
2024-03-20 20:52:34 -0400

    Add protected route for account page

--- pc-parts-app/src/App.tsx
+++ pc-parts-app/src/App.tsx
@@ -1,7 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthProvider } from "./components/context/AuthContext";
+import ProtectedRoute from "./components/context/ProtectedRoute";
 import { Footer, NavBar } from "./components/navigation";
 import { About } from "./pages/About";
+import { Account } from "./pages/Account";
 import { Landing } from "./pages/Landing";
 import { ProductPage } from "./pages/ProductPage";
 import { Store } from "./pages/Store";
@@ -9,18 +12,23 @@ import { Store } from "./pages/Store";
 function App() {
   return (
     <Router>
-      <Box bg={"#FAFAFA"} minH={"100vh"} minW={"100vw"}>
-        <NavBar />
-        <Box>
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/product/:productId" element={<ProductPage />} />
-          </Routes>
+      <AuthProvider>
+        <Box bg={"#FAFAFA"} minH={"100vh"} minW={"100vw"}>
+          <NavBar />
+          <Box>
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/product/:productId" element={<ProductPage />} />
+              <Route element={<ProtectedRoute />}>
+                <Route path="/account" element={<Account />} />
+              </Route>
+            </Routes>
+          </Box>
+          <Footer />
         </Box>
-        <Footer />
-      </Box>
+      </AuthProvider>
     </Router>
   );
 }
