ec7183b42c42e0f8e3d1e4e9fe75defa61f8d29a
nicolasjott <nicobox2002@gmail.com>
2024-04-02 23:34:17 -0400

    Add routes for getting, removing, and updating cart items

--- pc-parts-api/routes/api/cart.php
+++ pc-parts-api/routes/api/cart.php
@@ -7,4 +7,8 @@ Route::group(['prefix' => 'cart'], function () {
     Route::post('/', [CartController::class, 'createCart']);
     Route::get('/', [CartController::class, 'getCart']);
     Route::post('/item', [CartController::class, 'addCartItem']);
+    Route::get('/item/{id}', [CartController::class, 'getCartItem']);
+    Route::delete('/item/{id}', [CartController::class, 'removeCartItem']);
+    Route::put('/item/{id}', [CartController::class, 'updateCartItem']);
+
 });
