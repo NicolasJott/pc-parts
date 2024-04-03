4a465131823a6f1fef4d67c1948b6e802dfc15d9
nicolasjott <nicobox2002@gmail.com>
2024-04-02 23:34:06 -0400

    Add cart item manipulation methods to CartItemService

--- pc-parts-api/app/Services/CartItemService.php
+++ pc-parts-api/app/Services/CartItemService.php
@@ -13,6 +13,7 @@ class CartItemService
      * Create a new service instance.
      *
      * @param  CartItemRepository  $cartItemRepository
+     * @param 
      * @return void
      */
     public function __construct(private CartItemRepository $cartItemRepository)
@@ -34,6 +35,55 @@ class CartItemService
             'cart_id' => $cart_id,
             'product_id' => $request->product_id,
             'quantity' => $request->quantity
+        ]);
+    }
+
+    /**
+     * Get a cart item.
+     *
+     * @param  int  $id
+     * @return CartItem
+     */
+    public function getCartItem(int $id): CartItem
+    {
+        return $this->cartItemRepository->get(['id' => $id]);
+    }
+
+    /**
+     * Remove a cart item.
+     *
+     * @param  CartItem  $cartItem
+     * @return void
+     */
+    public function removeCartItem(CartItem $cartItem): void
+    {
+        $this->cartItemRepository->delete($cartItem);
+    }
+
+    /**
+     * Increment quantity to a cart item.
+     *
+     * @param  CartItem  $cartItem
+     * @return bool
+     */
+    public function addQuantity(CartItem $cartItem): bool
+    {
+        return $this->cartItemRepository->update($cartItem, [
+            'quantity' => $cartItem->quantity + 1
+        ]);
+
+    }
+
+    /**
+     * Decrement quantity to a cart item.
+     *
+     * @param  CartItem  $cartItem
+     * @return bool
+     */
+    public function removeQuantity(CartItem $cartItem): bool
+    {
+        return $this->cartItemRepository->update($cartItem, [
+            'quantity' => $cartItem->quantity - 1
         ]);
     }
 
