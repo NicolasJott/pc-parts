e14f389bf26d98fda6ea4e85eb894e7addf201ef
nicolasjott <nicobox2002@gmail.com>
2024-04-02 23:34:10 -0400

    Change total column data type to float in carts table

--- pc-parts-api/database/migrations/2024_03_28_030010_create_carts_table.php
+++ pc-parts-api/database/migrations/2024_03_28_030010_create_carts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->integer('total');
+            $table->float('total');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
