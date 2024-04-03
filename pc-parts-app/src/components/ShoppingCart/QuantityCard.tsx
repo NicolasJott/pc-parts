2978288a80203372f54bad3dfa99f5c38f922cd4
nicolasjott <nicobox2002@gmail.com>
2024-04-02 23:34:59 -0400

    Add quantity functionality to QuantityCard component

--- pc-parts-app/src/components/ShoppingCart/QuantityCard.tsx
+++ pc-parts-app/src/components/ShoppingCart/QuantityCard.tsx
@@ -1,7 +1,73 @@
 import { AddIcon, MinusIcon } from "@chakra-ui/icons";
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Spinner, Text } from "@chakra-ui/react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteCartItem, getCartItem, updateCartItem } from "../../api/cart";
 
-export const QuantityCard = () => {
+interface IQuantityCardProps {
+  cartItemId: number;
+}
+
+export const QuantityCard = ({ cartItemId }: IQuantityCardProps) => {
+  const queryClient = useQueryClient();
+
+  const query = useQuery({
+    queryKey: ["cartItemQuantity", cartItemId],
+    queryFn: () => getCartItem(cartItemId),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (method: "add" | "remove") =>
+      updateCartItem(cartItemId, method),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cartItemQuantity", cartItemId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["cart"],
+      });
+    },
+  });
+
+  const removeFromCart = useMutation({
+    mutationFn: (id: string | number) => deleteCartItem(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cart"],
+      });
+    },
+  });
+
+  if (query.isLoading) {
+    return (
+      <Box
+        border={"1px solid  #404040"}
+        px={4}
+        py={2}
+        color="white"
+        borderRadius={32}
+      >
+        <HStack spacing={5}>
+          <MinusIcon
+            fontSize={"10px"}
+            color={"lightText.700"}
+            cursor={"pointer"}
+          />
+          <Spinner size="xs" color="lightText.700" />
+          <AddIcon
+            fontSize={"10px"}
+            color={"lightText.700"}
+            justifySelf={"flex-end"}
+            cursor={"pointer"}
+          />
+        </HStack>
+      </Box>
+    );
+  }
+
+  if (query.isError) {
+    return <Box>Error...</Box>;
+  }
+
   return (
     <Box
       border={"1px solid  #404040"}
@@ -15,13 +81,28 @@ export const QuantityCard = () => {
           fontSize={"10px"}
           color={"lightText.700"}
           cursor={"pointer"}
+          onClick={() => {
+            if (query.data?.quantity === 1) {
+              removeFromCart.mutate(cartItemId);
+              return;
+            }
+            mutation.mutate("remove");
+          }}
         />
-        <Text>1</Text>
+        {removeFromCart.isPending || mutation.isPending || query.isLoading ? (
+          <Spinner size="sm" color="lightText.700" />
+        ) : (
+          <Text>{query.data?.quantity}</Text>
+        )}
+
         <AddIcon
           fontSize={"10px"}
           color={"lightText.700"}
           justifySelf={"flex-end"}
           cursor={"pointer"}
+          onClick={() => {
+            mutation.mutate("add");
+          }}
         />
       </HStack>
     </Box>
