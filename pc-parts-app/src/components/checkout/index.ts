1365f80042ae79609c3c812e1a2239f4a2abcf03
nicolasjott <nicobox2002@gmail.com>
2024-04-14 20:05:16 -0400

    Add new checkout components: ShippingForm and PaymentForm

--- pc-parts-app/src/components/checkout/index.ts
+++ pc-parts-app/src/components/checkout/index.ts
@@ -1,4 +1,8 @@
+export { CheckoutBreadCrumbs } from "./CheckoutBreadCrumbs";
 export { CheckoutItem } from "./CheckoutItem";
 export { CheckoutPage } from "./CheckoutPage";
 export { CheckoutPrice } from "./CheckoutPrice";
+export { InformationForm } from "./InformationForm";
 export { OrderSummary } from "./OrderSummary";
+export { PaymentForm } from "./PaymentForm";
+export { ShippingForm } from "./ShippingForm";
